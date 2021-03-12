74f06dc48d4c630eae0575a5cf38ffc1b61ea683
Max Yakunin <58250827+yakuninmax@users.noreply.github.com>
2021-03-12 12:26:04 +0300

    Trim some spaces

--- main.tf
+++ main.tf
@@ -11,10 +11,10 @@ resource "vcd_vapp_vm" "vm" {
   computer_name   = var.name
 
   override_template_disk {
-    size_in_mb      = var.system_disk_size * 1024
-    bus_type        = var.system_disk_bus
-    bus_number      = 0
-    unit_number     = 0
+    size_in_mb  = var.system_disk_size * 1024
+    bus_type    = var.system_disk_bus
+    bus_number  = 0
+    unit_number = 0
   }
   
   dynamic "network" {
@@ -59,10 +59,10 @@ resource "vcd_inserted_media" "media" {
   count      = var.media != null ? 1 : 0
   depends_on = [ vcd_vm_internal_disk.disk ]
 
-  vapp_name = vcd_vapp_vm.vm.vapp_name
-  vm_name   = vcd_vapp_vm.vm.name
-  catalog   = var.media.catalog
-  name      = var.media.name
+  vapp_name   = vcd_vapp_vm.vm.vapp_name
+  vm_name     = vcd_vapp_vm.vm.name
+  catalog     = var.media.catalog
+  name        = var.media.name
   eject_force = true
 }
 
@@ -70,16 +70,16 @@ resource "vcd_inserted_media" "media" {
 resource "random_integer" "ssh-port" {
   count = var.allow_external_ssh == true ? 1 : 0
   
-  min     = 40000
-  max     = 49999
+  min = 40000
+  max = 49999
 }
 
 # Get random RDP port
 resource "random_integer" "rdp-port" {
   count = var.allow_external_rdp == true ? 1 : 0
 
-  min     = 50000
-  max     = 59999
+  min = 50000
+  max = 59999
 }
 
 # SSH DNAT rule
@@ -165,13 +165,13 @@ resource "null_resource" "set-default-shell" {
   provisioner "remote-exec" {
     
     connection {
-      type            = "ssh"
-      user            = "Administrator"
-      password        = vcd_vapp_vm.vm.customization[0].admin_password
-      host            = var.allow_external_ssh == true ? var.external_ip != "" ? var.external_ip : data.vcd_edgegateway.edge.external_network_ips[0] : vcd_vapp_vm.vm.network[0].ip
-      port            = var.allow_external_ssh == true ? var.external_ssh_port != "" ? var.external_ssh_port : random_integer.ssh-port[0].result : 22
-      script_path     = "/Windows/Temp/terraform_%RAND%.cmd"
-      timeout         = "15m"
+      type        = "ssh"
+      user        = "Administrator"
+      password    = vcd_vapp_vm.vm.customization[0].admin_password
+      host        = var.allow_external_ssh == true ? var.external_ip != "" ? var.external_ip : data.vcd_edgegateway.edge.external_network_ips[0] : vcd_vapp_vm.vm.network[0].ip
+      port        = var.allow_external_ssh == true ? var.external_ssh_port != "" ? var.external_ssh_port : random_integer.ssh-port[0].result : 22
+      script_path = "/Windows/Temp/terraform_%RAND%.cmd"
+      timeout     = "15m"
     }
 
     inline = [
@@ -187,13 +187,13 @@ resource "null_resource" "initial-config" {
   provisioner "remote-exec" {
     
     connection {
-      type            = "ssh"
-      user            = "Administrator"
-      password        = vcd_vapp_vm.vm.customization[0].admin_password
-      host            = var.allow_external_ssh == true ? var.external_ip != "" ? var.external_ip : data.vcd_edgegateway.edge.external_network_ips[0] : vcd_vapp_vm.vm.network[0].ip
-      port            = var.allow_external_ssh == true ? var.external_ssh_port != "" ? var.external_ssh_port : random_integer.ssh-port[0].result : 22
-      script_path     = "/Windows/Temp/terraform_%RAND%.ps1"
-      timeout         = "15m"
+      type        = "ssh"
+      user        = "Administrator"
+      password    = vcd_vapp_vm.vm.customization[0].admin_password
+      host        = var.allow_external_ssh == true ? var.external_ip != "" ? var.external_ip : data.vcd_edgegateway.edge.external_network_ips[0] : vcd_vapp_vm.vm.network[0].ip
+      port        = var.allow_external_ssh == true ? var.external_ssh_port != "" ? var.external_ssh_port : random_integer.ssh-port[0].result : 22
+      script_path = "/Windows/Temp/terraform_%RAND%.ps1"
+      timeout     = "15m"
     }
 
     inline = [
@@ -217,13 +217,13 @@ resource "null_resource" "disk-config" {
   provisioner "remote-exec" {
     
     connection {
-      type            = "ssh"
-      user            = "Administrator"
-      password        = vcd_vapp_vm.vm.customization[0].admin_password
-      host            = var.allow_external_ssh == true ? var.external_ip != "" ? var.external_ip : data.vcd_edgegateway.edge.external_network_ips[0] : vcd_vapp_vm.vm.network[0].ip
-      port            = var.allow_external_ssh == true ? var.external_ssh_port != "" ? var.external_ssh_port : random_integer.ssh-port[0].result : 22
-      script_path     = "/Windows/Temp/terraform_%RAND%.ps1"
-      timeout         = "15m"
+      type        = "ssh"
+      user        = "Administrator"
+      password    = vcd_vapp_vm.vm.customization[0].admin_password
+      host        = var.allow_external_ssh == true ? var.external_ip != "" ? var.external_ip : data.vcd_edgegateway.edge.external_network_ips[0] : vcd_vapp_vm.vm.network[0].ip
+      port        = var.allow_external_ssh == true ? var.external_ssh_port != "" ? var.external_ssh_port : random_integer.ssh-port[0].result : 22
+      script_path = "/Windows/Temp/terraform_%RAND%.ps1"
+      timeout     = "15m"
     }
 
     inline = [
